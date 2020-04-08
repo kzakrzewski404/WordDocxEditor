3f9ec5ec63c96f24c4f6c539fb7cdba8126b5a58
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-08 19:07:57 +0200

    Ui scaffolding

--- WordDocxEditor/Form1.Designer.cs
+++ WordDocxEditor/Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker_Received = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_Responded = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Address = new System.Windows.Forms.TextBox();
+            this.comboBox_City = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown_Number = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Number)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data otrzymania wniosku:";
+            // 
+            // dateTimePicker_Received
+            // 
+            this.dateTimePicker_Received.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.dateTimePicker_Received.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker_Received.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_Received.Location = new System.Drawing.Point(16, 31);
+            this.dateTimePicker_Received.Name = "dateTimePicker_Received";
+            this.dateTimePicker_Received.Size = new System.Drawing.Size(185, 27);
+            this.dateTimePicker_Received.TabIndex = 20;
+            this.dateTimePicker_Received.TabStop = false;
+            // 
+            // dateTimePicker_Responded
+            // 
+            this.dateTimePicker_Responded.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.dateTimePicker_Responded.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker_Responded.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_Responded.Location = new System.Drawing.Point(248, 31);
+            this.dateTimePicker_Responded.Name = "dateTimePicker_Responded";
+            this.dateTimePicker_Responded.Size = new System.Drawing.Size(185, 27);
+            this.dateTimePicker_Responded.TabIndex = 21;
+            this.dateTimePicker_Responded.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label2.Location = new System.Drawing.Point(244, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data nadania odpowiedzi:";
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_Name.Location = new System.Drawing.Point(16, 203);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(200, 27);
+            this.textBox_Name.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label3.Location = new System.Drawing.Point(12, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Imię i nazwisko / Nazwa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label4.Location = new System.Drawing.Point(12, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ulica:";
+            // 
+            // textBox_Address
+            // 
+            this.textBox_Address.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_Address.Location = new System.Drawing.Point(16, 335);
+            this.textBox_Address.Name = "textBox_Address";
+            this.textBox_Address.Size = new System.Drawing.Size(200, 27);
+            this.textBox_Address.TabIndex = 3;
+            // 
+            // comboBox_City
+            // 
+            this.comboBox_City.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_City.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox_City.FormattingEnabled = true;
+            this.comboBox_City.Items.AddRange(new object[] {
+            "95-060 Brzeziny",
+            "95-061 Dmosin",
+            "95-063 Rogów",
+            "95-047 Jeżów"});
+            this.comboBox_City.Location = new System.Drawing.Point(16, 408);
+            this.comboBox_City.Name = "comboBox_City";
+            this.comboBox_City.Size = new System.Drawing.Size(200, 27);
+            this.comboBox_City.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label5.Location = new System.Drawing.Point(12, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Miejscowość:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.label6.Location = new System.Drawing.Point(12, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Numer:";
+            // 
+            // numericUpDown_Number
+            // 
+            this.numericUpDown_Number.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown_Number.Location = new System.Drawing.Point(16, 268);
+            this.numericUpDown_Number.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown_Number.Name = "numericUpDown_Number";
+            this.numericUpDown_Number.Size = new System.Drawing.Size(200, 27);
+            this.numericUpDown_Number.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(482, 536);
+            this.Controls.Add(this.numericUpDown_Number);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox_City);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_Address);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker_Received);
+            this.Controls.Add(this.dateTimePicker_Responded);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Number)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Received;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Responded;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Address;
+        private System.Windows.Forms.ComboBox comboBox_City;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Number;
     }
 }
 
