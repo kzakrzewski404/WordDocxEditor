c3046a6cbc9bb630a52cb99d36c8bfb2f3780bb7
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-08 23:26:57 +0200

    finished Generator, added tagsConfig

--- WordDocxEditor/Generator.cs
+++ WordDocxEditor/Generator.cs
@@ -12,6 +12,7 @@ namespace WordDocxEditor
         private static Application wordApp;
 
         private string _outputFilePath = "";
+        private Document _document;
 
 
         public void Generate(string sourceTemplatePath, GeneratorData data, bool doPrint = false, int numberOfCopies = 0)
@@ -21,8 +22,18 @@ namespace WordDocxEditor
             _outputFilePath = _desktopPath + data.Name;
             File.Copy(sourceTemplatePath, _outputFilePath);
 
-            Document doc = wordApp.Documents.Open(_outputFilePath, ReadOnly: false);
+            _document = wordApp.Documents.Open(_outputFilePath, ReadOnly: false);
+            ReplaceTag(TagsConfig.Name, data.Name);
+            ReplaceTag(TagsConfig.Address, data.Address);
+            ReplaceTag(TagsConfig.City, data.City);
+            ReplaceTag(TagsConfig.CaseId, data.CaseId.ToString());
+            ReplaceTag(TagsConfig.ReceivedDate, data.ReceivedDate.Date.ToString("dd.MM.yyyy"));
+            ReplaceTag(TagsConfig.RespondedDate, data.RespondedDate.Date.ToString("dd.MM.yyyy"));
 
+            _document.Save();
+
+            object missing = System.Reflection.Missing.Value;
+            _document.Close(ref missing);
         }
 
 
@@ -37,6 +48,12 @@ namespace WordDocxEditor
 
                 wordApp = new Application();
             }
+        }
+
+        private void ReplaceTag(string tag, string replacedText)
+        {
+            _document.Content.Find.Execute(tag, false, true, false, false, false, true, 1, false, replacedText, 2,
+            false, false, false, false);
         }
     }
 }
---
+++ WordDocxEditor/TagsConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace WordDocxEditor
+{
+    public static class TagsConfig
+    {
+        public static string Name = "<name>";
+        public static string Address = "<addr>";
+        public static string City = "<city>";
+        public static string CaseId = "<id>";
+        public static string ReceivedDate = "<dateIn>";
+        public static string RespondedDate = "<dateOut>";
+    }
+}
