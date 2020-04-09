0f3a42879980f2cd3e6fb3977e32f6733dfa3970
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-09 20:20:32 +0200

    Separated templatesLoader from Form

---
+++ WordDocxEditor/TemplatesLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace WordDocxEditor
+{
+    public class TemplatesLoader
+    {
+        public TemplatesLoaderSummary Load()
+        {
+            TemplatesLoaderSummary summary = new TemplatesLoaderSummary();
+            FolderBrowserDialog folderBrowserDialog = CreateFolderBrowserDialog();
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+                FileInfo[] files = directoryInfo.GetFiles("_*");
+
+                summary.AddFilePath(E_TemplateId.Mr, FindTemplateFile(files, "_pan_"));
+                summary.AddFilePath(E_TemplateId.Mrs, FindTemplateFile(files, "_pani_"));
+                summary.AddFilePath(E_TemplateId.Company, FindTemplateFile(files, "_firma_"));
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano folderu.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            return summary;
+        }
+
+
+        private FolderBrowserDialog CreateFolderBrowserDialog()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Wybierz folder z trzema szablonami";
+
+            return folderBrowserDialog;
+        }
+
+        private string FindTemplateFile(FileInfo[] files, string searchPattern)
+        {
+            return files.Where(x => x.Name.Contains(searchPattern)).Select(x => x.Name).FirstOrDefault();
+        }
+    }
+}
---
+++ WordDocxEditor/TemplatesLoaderSummary.cs
@@ -0,0 +1,29 @@
+﻿namespace WordDocxEditor
+{
+    public class TemplatesLoaderSummary
+    {
+        public string[] FilePaths { get; private set; }
+
+        public bool IsSuccess => CheckIfLoadedSuccessfully();
+
+
+        public TemplatesLoaderSummary() => FilePaths = new string[(int)E_TemplateId.ENUM_LENGTH];
+
+
+        public void AddFilePath(E_TemplateId id, string path) => FilePaths[(int)id] = path;
+
+
+        private bool CheckIfLoadedSuccessfully()
+        {
+            foreach (var item in FilePaths)
+            {
+                if (item == null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
