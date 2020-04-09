41146c7952e247918b650b4f34761e2a65b59acb
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-09 17:37:53 +0200

    added try catch to opening word file

--- WordDocxEditor/Generator.cs
+++ WordDocxEditor/Generator.cs
@@ -9,13 +9,13 @@ namespace WordDocxEditor
     {
         private static string _desktopPath = "";
         private static bool _isInitialized;
-        private static Application wordApp;
+        private static Application _wordApp;
 
         private string _outputFilePath = "";
         private Document _document;
 
 
-        public void Generate(string sourceTemplatePath, GeneratorData data, bool doPrint = false, int numberOfCopies = 0)
+        public bool Generate(string sourceTemplatePath, GeneratorData data, bool doPrint = false, int numberOfCopies = 0)
         {
             Initialize();
 
@@ -27,19 +27,45 @@ namespace WordDocxEditor
             _outputFilePath = _desktopPath + "\\" + data.Name + Path.GetExtension(sourceTemplatePath);
             File.Copy(sourceTemplatePath, _outputFilePath);
 
-            _document = wordApp.Documents.Open(_outputFilePath, ReadOnly: false);
-            ReplaceTag(TagsConfig.Name, data.Name);
-            ReplaceTag(TagsConfig.Address, data.Address);
-            ReplaceTag(TagsConfig.City, data.City);
-            ReplaceTag(TagsConfig.CaseId, data.CaseId.ToString());
-            ReplaceTag(TagsConfig.ReceivedDate, data.ReceivedDate.Date.ToString("dd.MM.yyyy"));
-            ReplaceTag(TagsConfig.RespondedDate, data.RespondedDate.Date.ToString("dd.MM.yyyy"));
+            bool hasTriedToLocalizeWordApp = false;
+            for (int i = 0; i < 2; i++)
+            {
+                try
+                {
+                    _document = _wordApp.Documents.Open(_outputFilePath, ReadOnly: false);
+                    ReplaceTag(TagsConfig.Name, data.Name);
+                    ReplaceTag(TagsConfig.Address, data.Address);
+                    ReplaceTag(TagsConfig.City, data.City);
+                    ReplaceTag(TagsConfig.CaseId, data.CaseId.ToString());
+                    ReplaceTag(TagsConfig.ReceivedDate, data.ReceivedDate.Date.ToString("dd.MM.yyyy"));
+                    ReplaceTag(TagsConfig.RespondedDate, data.RespondedDate.Date.ToString("dd.MM.yyyy"));
 
-            _document.Save();
-            _document.PrintOut(Copies: numberOfCopies);
+                    _document.Save();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    if (hasTriedToLocalizeWordApp)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        hasTriedToLocalizeWordApp = true;
+                        LocateWordApp();
+                    }
+                }
+            }
+
+            if (doPrint)
+            {
+                _document.PrintOut(Copies: numberOfCopies);
+            }
 
             object missing = System.Reflection.Missing.Value;
             _document.Close(ref missing);
+
+            return true;
         }
 
 
@@ -52,9 +78,11 @@ namespace WordDocxEditor
                 _desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
                        + $"\\Dokumenty - {DateTime.Today.ToString("yyyy.MM.dd")}";
 
-                wordApp = new Application();
+                LocateWordApp();
             }
         }
+
+        private void LocateWordApp() => _wordApp = new Application();
 
         private void ReplaceTag(string tag, string replacedText)
         {
