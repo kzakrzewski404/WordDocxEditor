cc2b3035dba8edd60b23f8022cb67fec34746059
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-15 19:02:53 +0200

    Create DirectoryPaths.cs

---
+++ WordDocxEditor/Common/DirectoryPaths.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace WordDocxEditor.Common
+{
+    public class DirectoryPaths
+    {
+        public string DesktopOutput => Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                       + $"\\Dokumenty - {DateTime.Today.ToString("yyyy.MM.dd")}";
+    }
+}
