2900d9a3925c17fbb75f80bdf02c39815d76af64
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-15 21:57:59 +0200

    added button disable on export

--- WordDocxEditor/HistoryView.cs
+++ WordDocxEditor/HistoryView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 
@@ -10,6 +11,10 @@ namespace WordDocxEditor
 {
     public partial class HistoryView : Form
     {
+        private UiCommon _common = new UiCommon();
+        private Exporter _exporter = new Exporter();
+
+
         public HistoryView()
         {
             InitializeComponent();
@@ -35,11 +40,27 @@ namespace WordDocxEditor
 
         private void button_ExportToExcel_Click(object sender, EventArgs e)
         {
-            Exporter exporter = new Exporter();
-            exporter.ExportToExcel(dataGridView1);
-            MessageBox.Show("Koniec");
+            SwitchButtons(false);
+
+            bool result = _exporter.ExportToExcel(dataGridView1);
+            if (result)
+            {
+                MessageBox.Show("Plik został zapisany.", "Potwierdzenie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                _common.ShowError("Wystąpił nieoczekiwany błąd podczas eksportu.");
+            }
+
+            SwitchButtons(true);
         }
 
         private void OnFormShowing(object sender, EventArgs e) => label_numberOfEntries.Text = dataGridView1.RowCount.ToString();
+
+        private void SwitchButtons(bool mode)
+        {
+            button_ExportToExcel.Enabled = mode;
+            button_ExportToExcel.BackColor = mode ? Color.FromArgb(0, 0, 0, 0) : Color.Green;
+        }
     }
 }
