23083c9866702ae00cf177dc46ae28d8afc48fea
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-15 21:31:45 +0200

    fixed excel not closing

--- WordDocxEditor/History/Exporter.cs
+++ WordDocxEditor/History/Exporter.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using Microsoft.Office.Interop.Excel;
 using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Excel;
+using ExcelApplication = Microsoft.Office.Interop.Excel.Application;
 
 using WordDocxEditor.Common;
 
@@ -14,86 +14,99 @@ namespace WordDocxEditor.History
 {
     public class Exporter
     {
-        private static string _desktopOutputDirectory = "";
-        private static bool _isInitialized;
-        private static Microsoft.Office.Interop.Excel.Application _excelApp;
-        private Worksheet _worksheet;
+        private string _desktopOutputDirectory = "";
+        private bool _isInitialized;
+        private ExcelApplication _excelApp;
+        private Worksheet _workSheet;
+        private Process[] _processesBeforeExporting;
 
-
-        public bool ExportToExcel(DataGridView source) => SaveData(source);
-
-
-        private void copyAlltoClipboard(DataGridView source)
-        {
-            source.SelectAll();
-            DataObject dataObj = source.GetClipboardContent();
-            if (dataObj != null)
-                Clipboard.SetDataObject(dataObj);
-        }
-
-
-        private void GenerateHeader(Worksheet workSheet, DataGridView source)
-        {
-            //todo: docelowy font ms san serif 10
-
-            for (int i = 0; i < source.Columns.Count; i++)
-            {
-                workSheet.Cells[1, i + 1] = source.Columns[i].HeaderText;
-                //workSheet.Columns[1, i + 1].Width = source.Columns[i].Width;
-                //((Range)workSheet.Cells[1, i]).EntireColumn.ColumnWidth = source.Columns[i - 1].Width;
-                ((Range)workSheet.Cells[1, i + 1]).EntireColumn.ColumnWidth = source.Columns[i].Width / 6;
-            }
-        }
-
-        private bool SaveData(DataGridView source)
-        {
+        public bool ExportToExcel(DataGridView data)
+        { 
             Initialize();
-
-            bool hasTriedToLocalizeExcelApp = false;
-            for (int t = 0; t < 2; t++)
+            _processesBeforeExporting = Process.GetProcessesByName("EXCEL");
+            string outputFilePath = _desktopOutputDirectory + $"\\Historia wygenerowanych plików - " +
+                                                              $"{DateTime.Now.ToString("H.mm.ss - dd.MM.yyyy")}.xlsx";
+            if (TryOpenExcelApp() && TryCreateWorkSheet())
             {
                 try
                 {
-                    Workbook book;
-                    Worksheet sheet;
-                    object missingValue = System.Reflection.Missing.Value;
-                    book = _excelApp.Workbooks.Add(missingValue);
-                    sheet = (Worksheet)book.Worksheets.get_Item(1);
-                    _excelApp.Visible = true;
-
-                    GenerateHeader(sheet, source);
-                    //// storing header part in Excel  
-                    //for (int i = 1; i < source.Columns.Count + 1; i++)
-                    //{
-                    //    sheet.Cells[1, i] = source.Columns[i - 1].HeaderText;
-                    //}
-                    // storing Each row and column value to excel sheet  
-                    for (int i = 0; i < source.Rows.Count; i++)
-                    {
-                        for (int j = 0; j < source.Columns.Count; j++)
-                        {
-                            sheet.Cells[i + 2, j + 1] = source.Rows[i].Cells[j].Value.ToString();
-                        }
-                    }
-                    break;
+                    GenerateHeader(data);
+                    GenerateContent(data);
+                    _workSheet.SaveAs(outputFilePath);
+                    CloseExcelApp();
+                    return true;
                 }
-                catch (Exception e)
-                {
-                    if (hasTriedToLocalizeExcelApp)
-                    {
-                        CloseExcelFile();
-                        return false;
-                    }
-                    else
-                    {
-                        hasTriedToLocalizeExcelApp = true;
-                        LocalizeExcelApp();
-                        continue;
-                    }
-                }
+                catch { /* Empty */ }
             }
 
-            return true;
+            CloseExcelApp();
+            return false;
+        }
+
+
+        private bool TryOpenExcelApp()
+        {
+            try
+            {
+                _excelApp = new ExcelApplication();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private bool TryCreateWorkSheet()
+        {
+            try
+            {
+                Workbooks books = _excelApp.Workbooks; // <-- The important part
+                Workbook book = books.Add(System.Reflection.Missing.Value);
+
+                //Workbook book = _excelApp.Workbooks.Add(System.Reflection.Missing.Value);
+                _workSheet = (Worksheet)book.Worksheets.get_Item(1);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void CloseExcelApp()
+        {
+            _excelApp?.Workbooks.Close();
+            _excelApp?.Quit();
+
+            Process[] processesAfterExporting = Process.GetProcessesByName("EXCEL");
+            List<Process> exporterProcess = processesAfterExporting.Where(x => !_processesBeforeExporting.Any(y => x.Id == y.Id)).ToList();
+
+            foreach (Process proc in exporterProcess)
+            {
+                proc.Kill();
+            }
+        }
+
+        private void GenerateHeader(DataGridView data)
+        {
+            for (int i = 0; i < data.Columns.Count; i++)
+            {
+                _workSheet.Cells[1, i + 1] = data.Columns[i].HeaderText;
+                ((Range)_workSheet.Cells[1, i + 1]).EntireColumn.ColumnWidth = data.Columns[i].Width * 0.14f;
+                ((Range)_workSheet.Cells[1, i + 1]).Font.Bold = true;
+            }
+        }
+
+        private void GenerateContent(DataGridView data)
+        {
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                for (int j = 0; j < data.Columns.Count; j++)
+                {
+                    _workSheet.Cells[i + 2, j + 1] = data.Rows[i].Cells[j].Value.ToString();
+                }
+            }
         }
 
         private void Initialize()
@@ -102,39 +115,7 @@ namespace WordDocxEditor.History
             {
                 _isInitialized = true;
                 _desktopOutputDirectory = new DirectoryPaths().DesktopOutput;
-                LocalizeExcelApp();
             }
-        }
-
-        private void LocalizeExcelApp()
-        {
-            _excelApp = new Microsoft.Office.Interop.Excel.Application();
-            //_excelApp.StandardFont = "Arial";
-            //_excelApp.StandardFontSize = 11;
-        }
-
-        private void CloseExcelFile()
-        {
-            object missing = System.Reflection.Missing.Value;
-            _excelApp.Quit();
-        }
-
-        private bool CanCreateFile(string targetFilePath)
-        {
-            if (File.Exists(targetFilePath))
-            {
-                DialogResult result = MessageBox.Show($"Plik {Path.GetFileName(targetFilePath)} już istnieje, nadpisać?",
-                                                      "Plik już istnieje", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (result == DialogResult.Yes)
-                {
-                    File.Delete(targetFilePath);
-                    return true;
-                }
-
-                return false;
-            }
-
-            return true;
         }
     }
 }
