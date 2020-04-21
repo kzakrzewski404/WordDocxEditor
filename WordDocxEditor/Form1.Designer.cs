74036e60df0a3010bd789c3fbb3aad1767966ee6
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-21 21:08:22 +0200

    fixed naming in combobox

--- WordDocxEditor/Form1.Designer.cs
+++ WordDocxEditor/Form1.Designer.cs
@@ -78,7 +78,6 @@
             this.folderDocelowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button_history = new System.Windows.Forms.Button();
             this.button_outputDir = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -179,7 +178,8 @@
             "95-060 Brzeziny",
             "95-061 Dmosin",
             "95-063 Rogów",
-            "95-047 Jeżów"});
+            "95-047 Jeżów",
+            "<Dodaj>"});
             this.comboBox_City.Location = new System.Drawing.Point(260, 175);
             this.comboBox_City.Name = "comboBox_City";
             this.comboBox_City.Size = new System.Drawing.Size(185, 27);
@@ -237,7 +237,6 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button_loadTemplates);
             this.groupBox1.Controls.Add(this.label_CompanyTemplate);
             this.groupBox1.Controls.Add(this.label_MrsTemplate);
@@ -636,16 +635,6 @@
             this.button_outputDir.UseVisualStyleBackColor = true;
             this.button_outputDir.Click += new System.EventHandler(this.button_outputDir_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(235, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 36;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -735,7 +724,6 @@
         private System.Windows.Forms.ToolStripMenuItem folderDocelowyToolStripMenuItem;
         private System.Windows.Forms.Button button_history;
         private System.Windows.Forms.Button button_outputDir;
-        private System.Windows.Forms.Button button1;
     }
 }
 
