aa0ff467a7ca0617eb73f12f8b3da77916d11fd4
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-21 20:10:43 +0200

    Update CityVerifier.cs

--- WordDocxEditor/CustomCity/CityVerifier.cs
+++ WordDocxEditor/CustomCity/CityVerifier.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+
+using WordDocxEditor.Common;
+
 
 namespace WordDocxEditor.CustomCity
 {
-    class CityVerifier
+    public class CityVerifier
     {
+        public VerifierResult Verify(string postalCode, string cityName)
+        {
+            if (!VerifyPostalCodeLength(postalCode))
+            {
+                return new VerifierResult("Nieprawidłowa długość kodu pocztowego.");
+            }
+            if (!VerifyPostalCodePattern(postalCode)
+            {
+                return new VerifierResult("Kod pocztowy nie jest w formacie \"XX-XXX\".");
+            }
+            if (!VerifyCity(cityName))
+            {
+                return new VerifierResult("Niepoprawna nazwa miasta.");
+            }
+
+            return new VerifierResult();
+        }
+
+
+        private bool VerifyPostalCodeLength(string postalCode) => (NotNull(postalCode) && postalCode.Length == 6);
+
+        private bool VerifyPostalCodePattern(string postalCode) => (NotNull(postalCode) && Regex.IsMatch(postalCode, @"\d{2}-\d{3}"));
+
+        private bool VerifyCity(string city) => NotNull(city);
+
+        private bool NotNull(string str) => !string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
     }
 }
