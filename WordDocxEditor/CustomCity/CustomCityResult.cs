61c80db66b01c58657fe0d63df70e2104c16430c
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-21 19:55:25 +0200

    added customCity classes

---
+++ WordDocxEditor/CustomCity/CustomCityResult.cs
@@ -0,0 +1,15 @@
+﻿namespace WordDocxEditor.CustomCity
+{
+    public class CustomCityResult
+    {
+        public bool IsCreatedSuccessful { get; private set; }
+        public string City { get; private set; }
+
+
+        public CustomCityResult(string postalCode, string cityName)
+        {
+            IsCreatedSuccessful = true;
+            City = $"{postalCode} - {cityName}";
+        }
+    }
+}
