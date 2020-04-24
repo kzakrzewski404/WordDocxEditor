b3cb60804d625618507f4001cadf5c17231a5d51
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-24 21:29:16 +0200

    created LoadedTemplates for handling loading from file

---
+++ WordDocxEditor/Main/Areas/Templates/LoadedTemplates.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace WordDocxEditor.Main.Areas.Templates
+{
+    public class LoadedTemplates
+    {
+        private Dictionary<TemplateId, string> _filePaths = new Dictionary<TemplateId, string>();
+
+
+        public string Name { get; private set; }
+
+
+        public LoadedTemplates(string templateDirectoryPath)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(templateDirectoryPath);
+            FileInfo[] files = directoryInfo.GetFiles("_*");
+
+            _filePaths.Add(TemplateId.Mr, FindTemplateFile(files, "_pan_"));
+            _filePaths.Add(TemplateId.Mrs, FindTemplateFile(files, "_pani_"));
+            _filePaths.Add(TemplateId.Company, FindTemplateFile(files, "_firma_"));
+
+            Name = directoryInfo.Name;
+        }
+
+
+        public string GetFilePath(TemplateId id) => _filePaths.Where(x => x.Key == id).Select(x => x.Value).FirstOrDefault();
+
+
+        private string FindTemplateFile(FileInfo[] files, string searchPattern)
+        {
+            return files.Where(x => x.Name.Contains(searchPattern)).Select(x => x.FullName).FirstOrDefault();
+        }
+    }
+}
