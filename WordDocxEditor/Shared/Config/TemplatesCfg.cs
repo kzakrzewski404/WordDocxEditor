b4b87c2a2e478ca1881b6326f21cd126e3417be0
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-25 20:44:42 +0200

    Update TemplatesCfg.cs

--- WordDocxEditor/Shared/Config/TemplatesCfg.cs
+++ WordDocxEditor/Shared/Config/TemplatesCfg.cs
@@ -5,5 +5,7 @@
         public static string MrHeader = "_pan_";
         public static string MrsHeader = "_pani_";
         public static string CompanyHeader = "_firma_";
+
+        public static string ArchivableTemplateName = "praca.gov.pl";
     }
 }
