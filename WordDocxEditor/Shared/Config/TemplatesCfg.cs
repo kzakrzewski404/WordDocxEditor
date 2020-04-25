d85620f7acf0a8b4c583d0dae0af75eda70a75dc
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-25 19:57:13 +0200

    Create TemplatesCfg.cs

---
+++ WordDocxEditor/Shared/Config/TemplatesCfg.cs
@@ -0,0 +1,9 @@
+﻿namespace WordDocxEditor.Shared.Config
+{
+    public static class TemplatesCfg
+    {
+        public static string MrHeader = "_pan_";
+        public static string MrsHeader = "_pani_";
+        public static string CompanyHeader = "_firma_";
+    }
+}
