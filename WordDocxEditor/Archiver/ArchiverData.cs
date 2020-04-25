ce835126f711ef3119f93467a8a6bf697ec3c095
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-25 22:07:57 +0200

    Create ArchiverData.cs

---
+++ WordDocxEditor/Archiver/ArchiverData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+
+namespace WordDocxEditor.Archiver
+{
+    public class ArchiverData
+    {
+        public string Name { get; private set; }
+        public List<string> Files { get; private set; }
+
+        public int NumberOfFiles => Files.Count;
+
+
+        public ArchiverData(string templateName)
+        {
+            Name = templateName;
+            Files = new List<string>();
+        }
+
+
+        public void AddFile(string fullFilePath) => Files.Add(fullFilePath);
+    }
+}
