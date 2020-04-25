f6ec69eabc814fb56bec0f36baa0c976d55f8787
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-25 19:57:08 +0200

    Create Directories.cs

---
+++ WordDocxEditor/Shared/Config/DirectoriesCfg.cs
@@ -0,0 +1,8 @@
+﻿namespace WordDocxEditor.Shared.Config
+{
+    public static class DirectoriesCfg
+    {
+        public static string Templates = "Szablony";
+        public static string Archive = @"K:\Archiwum";
+    }
+}
