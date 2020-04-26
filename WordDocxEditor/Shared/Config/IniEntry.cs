52ef3b9314414ea624468b56e08250aa3afac249
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-04-26 22:09:17 +0200

    Create IniEntry.cs

---
+++ WordDocxEditor/Shared/Config/IniEntry.cs
@@ -0,0 +1,19 @@
+﻿namespace WordDocxEditor.Shared.Config
+{
+    public class IniEntry
+    {
+        public IniEntry_Id Id { get; private set; }
+        public string Entry { get; private set; }
+        public string Value { get; private set; }
+
+
+        public IniEntry(IniEntry_Id id, string entry, string value)
+        {
+            Id = id;
+            Entry = entry;
+            Value = value;
+        }
+
+        public void ChangeValue(string value) => Value = value;
+    }
+}
